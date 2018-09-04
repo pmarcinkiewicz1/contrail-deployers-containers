9db7829fc380a365852cb81b49a67fc1b26fce89
Andrey Pavlov <andrey.mp@gmail.com>
2018-09-04 12:32:23 +0300

    rework openshift-ansible

--- containers/kolla-ansible-deployer/src/prepare.sh
+++ containers/kolla-ansible-deployer/src/prepare.sh
@@ -3,5 +3,5 @@
 yum install -y git
 
 cd /root
-[ -d contrail-ansible-deployer ] || git clone https://github.com/Juniper/contrail-ansible-deployer
-[ -d contrail-kolla-ansible ] || git clone https://github.com/Juniper/contrail-kolla-ansible
+[ -d contrail-ansible-deployer ] || git clone https://github.com/Juniper/contrail-ansible-deployer -b master
+[ -d contrail-kolla-ansible ] || git clone https://github.com/Juniper/contrail-kolla-ansible -b master
