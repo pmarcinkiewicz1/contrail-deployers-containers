92ecfea3aecd3c47bff9608129d96be093d097c5
Leela Bharath Kumar Kassetti <bharatkk@juniper.net>
2018-11-26 15:32:02 -0800

    Helm deployer containerization resources
    Patch 3:
    Moved pip package installation to single line
    Patch 2:
    Updated Container Base image. Modified package installation to use yum instead of apt-get
    
    Change-Id: I2a3761d2b2e2db634c239708d01d8d47cd9f4a69
    Partial-Bug: #1783723
    (cherry picked from commit 97442213df0ceb91b5c81eeaf5dd64709b141eff)

---
+++ containers/helm-deployer/src/README.md
@@ -0,0 +1,2 @@
+This folder contains openstack-helm, openstack-helm-infra and contrail-helm-deployer repositories.
+They can be used to deploy Contrail + OpenStack ( https://github.com/Juniper/contrail-helm-deployer/blob/master/doc/contrail-osh-multinode-install.md ).
---
+++ containers/helm-deployer/src/prepare.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+yum install -y git
+
+cd /root
+[ -d contrail-helm-deployer ] || git clone https://github.com/Juniper/contrail-helm-deployer -b master
+[ -d openstack-helm ] || git clone https://github.com/Juniper/openstack-helm -b master
+[ -d openstack-helm-infra ] || git clone https://github.com/Juniper/openstack-helm-infra -b master
