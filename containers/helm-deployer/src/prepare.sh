b40388dc749219536e0b4c4e0982084faaf24e07
Andrey Pavlov <andrey.mp@gmail.com>
2018-12-04 20:35:18 +0100

    change branch specification for R5.0
    Change-Id: I15ea4e4fe9971a6a45eca4a8e8f00c2fec592e36
    Partial-Bug: #1783723

--- containers/helm-deployer/src/prepare.sh
+++ containers/helm-deployer/src/prepare.sh
@@ -3,6 +3,6 @@
 yum install -y git
 
 cd /root
-[ -d contrail-helm-deployer ] || git clone https://github.com/Juniper/contrail-helm-deployer -b master
-[ -d openstack-helm ] || git clone https://github.com/Juniper/openstack-helm -b master
-[ -d openstack-helm-infra ] || git clone https://github.com/Juniper/openstack-helm-infra -b master
+[ -d contrail-helm-deployer ] || git clone https://github.com/Juniper/contrail-helm-deployer -b R5.0
+[ -d openstack-helm ] || git clone https://github.com/Juniper/openstack-helm -b R5.0
+[ -d openstack-helm-infra ] || git clone https://github.com/Juniper/openstack-helm-infra -b R5.0
