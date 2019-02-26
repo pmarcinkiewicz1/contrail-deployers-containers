1d9e99bf8f37494abaef07ea4107019d91c27388
Jakub Bielecki <jakub.bielecki@codilime.com>
2019-02-26 18:05:45 +0100

    change docker push after build to be optional
    Previously there was always a docker push in build.sh.
    Pushing will remain as default behavior.
    
    With authenticated registry (Nexus) for CI, we cannot push without
    a login secret.
    Here, we will disable docker push with CONTRAIL_REGISTRY_PUSH=0.
    The secret is only present in post-run stage of CI, where we will
    do a separate push logic.
    
    Use variable instead of the existence-of-logfile to gather the status.
    
    Change-Id: I06ae8679d57fe1053e46be2644b77cb38e9d1a9a
    Partial-Jira-bug: JD-428

--- parse-env.sh
+++ parse-env.sh
@@ -15,5 +15,6 @@ export LINUX_DISTR_VER=${LINUX_DISTR_VER:-'7.5.1804'}
 default_interface=`ip route show | grep "default via" | awk '{print $5}'`
 host_ip=`ip address show dev $default_interface | head -3 | tail -1 | tr "/" " " | awk '{print $2}'`
 export CONTRAIL_REGISTRY=${CONTRAIL_REGISTRY:-"$host_ip:5000"}
+export CONTRAIL_REGISTRY_PUSH=${CONTRAIL_REGISTRY_PUSH:-'1'}
 export CONTRAIL_DEPLOYERS_TAG=${CONTRAIL_DEPLOYERS_TAG:-${CONTRAIL_VERSION:-'5.0'}}
 export DEPLOYERS_BASE_CONTAINER=${DEPLOYERS_BASE_CONTAINER:-"$LINUX_DISTR:$LINUX_DISTR_VER"}
