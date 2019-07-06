b9ba5cc5ec4cf956137003cd4a01b239c43c37cb
alexey-mr <alexey.morlang@gmail.com>
2019-07-06 14:03:40 +0300

    Fixed all-deployers make target
    - to avoid run of another instance of make
    It is to be able to use correctly external targets
    like contrail-general-base
    
    Change-Id: I845c007c3d68338ae7bf3a4fdeb38ae79c4c57bd

--- containers/deployers-base/module.mk
+++ containers/deployers-base/module.mk
@@ -5,4 +5,4 @@ TARGET_NAME := $(lastword $(subst /, ,$(dir $(abspath $(lastword $(MAKEFILE_LIST
 deployer-$(TARGET_NAME): $(DB_DEPS)
 	@$(DEPLOYERS_DIR)/build.sh $(subst deployer-, ,$@)
 
-.PHONY: $(TARGET_NAME)
+.PHONY: deployer-$(TARGET_NAME)
--- containers/helm-deployer/module.mk
+++ containers/helm-deployer/module.mk
@@ -3,5 +3,7 @@ TARGET_NAME := $(lastword $(subst /, ,$(dir $(abspath $(lastword $(MAKEFILE_LIST
 deployer-$(TARGET_NAME): deployer-deployers-base
 	@$(DEPLOYERS_DIR)/build.sh $(subst deployer-, ,$@)
 
-.PHONY: $(TARGET_NAME)
+ALL_DEPLOERS := ${ALL_DEPLOERS} deployer-$(TARGET_NAME)
+
+.PHONY: deployer-$(TARGET_NAME)
 
