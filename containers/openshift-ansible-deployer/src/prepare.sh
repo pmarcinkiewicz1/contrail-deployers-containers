4b4309a0aa6ad9b2d6205b79b4ae4e4bf8893d83
alexey-mr <alexey.morlang@gmail.com>
2019-09-20 17:05:28 +0300

    Dont use git in case if folders are external
    Change-Id: I74d826aedd40c3b386bfe573e14628c702d125bb
    partial-jira-bug: CEM-8885

--- containers/openshift-ansible-deployer/src/prepare.sh
+++ containers/openshift-ansible-deployer/src/prepare.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -e
 
 cd /root
-[ -d openshift-ansible ] || git clone https://github.com/Juniper/openshift-ansible -b master
+if [ ! -d openshift-ansible ] ; then 
+    git clone https://github.com/Juniper/openshift-ansible -b master
 
-rm -rf openshift-ansible3.7
-cp -R openshift-ansible openshift-ansible3.7
-pushd openshift-ansible3.7
-git checkout release-3.7-contrail || git checkout origin/release-3.7-contrail
-popd
+    rm -rf openshift-ansible3.7
+    cp -R openshift-ansible openshift-ansible3.7
+    pushd openshift-ansible3.7
+    git checkout release-3.7-contrail || git checkout origin/release-3.7-contrail
+    popd
 
-rm -rf openshift-ansible3.9
-cp -R openshift-ansible openshift-ansible3.9
-pushd openshift-ansible3.9
-git checkout release-3.9-contrail || git checkout origin/release-3.9-contrail
-popd
+    rm -rf openshift-ansible3.9
+    cp -R openshift-ansible openshift-ansible3.9
+    pushd openshift-ansible3.9
+    git checkout release-3.9-contrail || git checkout origin/release-3.9-contrail
+    popd
+fi
