d4b700b0a7b98ecf770f3515f2e5e94d5c5f4ad2
Ivan Timofeev <ivanv.timofeev@gmail.com>
2019-11-25 11:48:17 +0300

    Always do clone as a temporaiy WA for tf-dev-env
    Change-Id: Iaab165fb3936f613fb2ae7e94ece805914d8956d

--- containers/kolla-ansible-deployer/src/prepare.sh
+++ containers/kolla-ansible-deployer/src/prepare.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+
 cd /root
+
+#HACK: always do clone as a temporary WA for tf-dev-env issue with symlinks in .git that are created by repo tool
+rm -rf contrail-ansible-deployer contrail-kolla-ansible
+
 [ -d contrail-ansible-deployer ] || git clone https://github.com/Juniper/contrail-ansible-deployer -b master
 [ -d contrail-kolla-ansible ] || git clone https://github.com/Juniper/contrail-kolla-ansible -b master
